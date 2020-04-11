6fc26474bad513bd8da9571972c1e5b24cfc6c01
James Batsell <jamesbatsell05@gmail.com>
2020-04-11 17:49:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,4 +2,16 @@ class Application
   
   @@items = []
   
-  
+    def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/items/#{name}"
+      resp.write "Here is the #{price}"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
