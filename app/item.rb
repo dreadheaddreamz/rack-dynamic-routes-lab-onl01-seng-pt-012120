a2e37a9168708a10a49b1302ba3323a657c6efc6
James Batsell <jamesbatsell05@gmail.com>
2020-04-11 17:49:14 +0000

    Automatically backed up by Learn

--- app/item.rb
+++ app/item.rb
@@ -6,18 +6,4 @@ class Item
     @price = price
   end
   
-   def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
- 
-    if req.path=="/items/#{name}"
-      resp.write "Here is the #{price}"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
- 
-    resp.finish
-  end
-  
 end
