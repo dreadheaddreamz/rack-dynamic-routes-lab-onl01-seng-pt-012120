0688a014a2d513a48d2ba3fc12d386323f6018eb
James Batsell <jamesbatsell05@gmail.com>
2020-04-11 17:53:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
     resp = Rack::Response.new
 
-    if req.path.match(/items\/.+/)
+    if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.detect { |i| i.name == item_name }
       if item
